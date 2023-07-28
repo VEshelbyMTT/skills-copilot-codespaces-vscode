5c443441ac3709a7969efafe31bcf064e517219c
Victoria <104829457+VEshelbyMTT@users.noreply.github.com>
2023-07-28 11:15:29 +0200

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,4 +1,5 @@
-// Create web sever - control + enter to retrieve copilot funsies
+// Create web sever 
+//control + enter to retrieve copilot funsies
 // 1. npm init -y
 // 2. npm install express
 // 3. npm install ejs
