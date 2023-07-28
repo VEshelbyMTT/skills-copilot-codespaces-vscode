38795916d1ef4afde3dfda5b11fd2a6b2c89b5ef
Victoria <104829457+VEshelbyMTT@users.noreply.github.com>
2023-07-28 09:07:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var skill = document.getElementById("skill");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var skillsValue = skills.options[skills.selectedIndex].value;
+    var skillsText = skills.options[skills.selectedIndex].text;
+    var skillValue;
+}
